c031b53d3da212682bfaadbfb50a576665e6ed46
Saurabhjk15 <jksharma6367dlp@gmail.com>
2025-06-25 06:44:18 +0530

    implement navigation bar

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import {Button} from "@/components/ui/button";
 const Page = () => {
   return (
     <div>
-      <h1 className="text-2xl underline ">Welcome to my saas application</h1>
+      <h1 className="text-2xl underline ">Popular Companions</h1>
 <Button>Lets get started</Button>
     </div>
   )
